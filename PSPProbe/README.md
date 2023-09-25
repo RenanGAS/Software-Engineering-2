c064f19cdcf7cb93a8ae1eb539b30da9ba8d66f7
RenanGAS <renansakaoki@gmail.com>
2023-09-25 12:48:47 -0300

    feat: start the PSPProbe program

---
+++ PSPProbe/README.md
@@ -0,0 +1,9 @@
+Para compilação:
+
+- javac src/Main.java src/tools/*.java
+
+Para execução:
+
+Para checagem de estilo:
+
+- java -jar checkstyle-10.12.3-all.jar -c bcc_engsw2_checks.xml ./src
---
+++ PSPProbe/src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+public final class Main {
+        private Main() {
+                throw new IllegalStateException("Utility class");
+        }
+
+        public static void main(String[] args) {
+                System.out.println("Hello world");
+        }
+}
